66f15c9f09715e7880ae904cf7e0758db1ab14e5
Gamal1tv <115107398+Gamal1tv@users.noreply.github.com>
2023-06-19 17:04:14 -0500

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TextToBinaryConversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblBox1 = new System.Windows.Forms.Label();
+            this.lblBox2 = new System.Windows.Forms.Label();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(470, 22);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(318, 416);
+            this.txtBox2.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(360, 183);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 51);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblBox1
+            // 
+            this.lblBox1.AutoSize = true;
+            this.lblBox1.Location = new System.Drawing.Point(12, 6);
+            this.lblBox1.Name = "lblBox1";
+            this.lblBox1.Size = new System.Drawing.Size(28, 13);
+            this.lblBox1.TabIndex = 3;
+            this.lblBox1.Text = "Text";
+            // 
+            // lblBox2
+            // 
+            this.lblBox2.AutoSize = true;
+            this.lblBox2.Location = new System.Drawing.Point(467, 6);
+            this.lblBox2.Name = "lblBox2";
+            this.lblBox2.Size = new System.Drawing.Size(36, 13);
+            this.lblBox2.TabIndex = 4;
+            this.lblBox2.Text = "Binary";
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(360, 240);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 51);
+            this.btnSwitch.TabIndex = 5;
+            this.btnSwitch.Text = "Switch";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(12, 22);
+            this.txtBox1.Multiline = true;
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(318, 416);
+            this.txtBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.lblBox2);
+            this.Controls.Add(this.lblBox1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtBox2);
+            this.Name = "Form1";
+            this.Text = "Binary Text Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblBox1;
+        private System.Windows.Forms.Label lblBox2;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.TextBox txtBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextToBinaryConversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            List<int> decimalValues = new List<int>(); //creates list
+
+            if (lblBox1.Text == "Text") //if lblBox1 says "Text" execute
+            {
+                txtBox2.Text = ""; //clear txtBox2
+                string textToConvert = txtBox1.Text; //textToConvert variable equals the text in txtBox1
+                foreach (char character in textToConvert) //for each character in textToConvert execute
+                {
+                    int charValues = (int)character; //character is converted to its deciaml value
+                    decimalValues.Add(charValues); //decimal value is add to list
+                    txtBox2.Text += charValues.ToString() + Environment.NewLine; //decimal value is added to txtBox2 along with new line
+                    string[] txtArray = txtBox2.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries); //array "txtarray" is created which equals the text in txtBox2 split by each new line
+                    txtBox2.Text = ""; //txtBox2 is cleared
+                    
+                }
+                foreach (int line in decimalValues) //for each integar in list execute
+                {
+                    int value = Convert.ToInt32(line); //integar value "value" is created and is equal to the number on each line of list "decimalValues"
+                    string binary = Convert.ToString(value, 2); //string value "binary" is created and is equal to the "value" variable converted to binary 
+                    txtBox2.Text += binary + " "; //txtBox2 shows binary plus a space behing it
+                }
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            if (lblBox1.Text == "Text") //if lblBox1 says "Text" execute
+            {
+                lblBox1.Text = "Binary"; //lblBox1 changes to say "Binary"
+                lblBox2.Text = "Text";//lblBox2 changes to say "Text"
+                string textInBox1 = txtBox1.Text; //string varible "textInBox1" is equal to the text in txtBox1
+                string textInBox2 = txtBox2.Text; //string varible "textInBox2" is equal to the text in txtBox2
+                txtBox1.Text = textInBox2; //text in txtBox1 is changed to equal textInBox2
+                txtBox2.Text = textInBox1;//text in txtBox2 is changed to equal textInBox1
+            }
+            else //if lblBox1 doesn't say "Text" execute
+            {
+                lblBox1.Text = "Text"; //lblBox1 changes to say "Text"
+                lblBox2.Text = "Binary"; //lblBox2 changes to say "Binary"
+                string textInBox1 = txtBox1.Text; //string varible "textInBox1" is equal to the text in txtBox1
+                string textInBox2 = txtBox2.Text; //string varible "textInBox2" is equal to the text in txtBox2
+                txtBox1.Text = textInBox2;//text in txtBox1 is changed to equal textInBox2
+                txtBox2.Text = textInBox1;//text in txtBox2 is changed to equal textInBox1
+            }
+        }
+
+    }
+}
